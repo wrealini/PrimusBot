935f269bec027c390e9ca88fa65b5673b8af8b19
Wallace Realini III <wwwrealini@gmail.com>
2026-04-27 21:50:56 -0700

    Updated test.py to wait for a new email to arrive using idle() and then print out only the message body, which for automatic emails to the primus discord bot gmail account sent via gsheet scripts is only the google sheet's spreadsheet id which can be used to read the sheet via the google sheets API

--- test.py
+++ test.py
@@ -13,8 +13,12 @@ def cb(cb_arg_list):
     for field in data:
         if type(field) is not tuple:
             continue
-        print('Message %s:\n%s\n'
-            % (field[0].split()[0], field[1]))
+        messagebody = str(field[1])
+        messagebody = messagebody.split('\\r\\n')
+        messagebody = messagebody[-2]
+        print(messagebody)
+        # print('Message %s:\n%s\n'
+        #     % (field[0].split()[0], field[1]))
 
 import getpass, imaplib2
 M = imaplib2.IMAP4_SSL(SERVER)
@@ -23,7 +27,8 @@ M.SELECT(readonly=True)
 print("Going into idle...")
 M.idle(timeout=None)
 typ, data = M.SEARCH(None, 'ALL')
-for num in data[0].split():
-    M.FETCH(num, '(RFC822)', callback=cb)
+M.FETCH(data[0].split()[-1], '(RFC822)', callback=cb)
+# for num in data[0].split():
+    # M.FETCH(num, '(RFC822)', callback=cb)
 M.CLOSE()
 M.LOGOUT()
