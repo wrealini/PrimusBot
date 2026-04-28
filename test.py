4816a28184713df1e8d8684f3db1835676751623
Wallace Realini III <wwwrealini@gmail.com>
2026-04-27 19:28:59 -0700

    Updated test.py to test imaplib2 idle() function

--- test.py
+++ test.py
@@ -1,48 +1,9 @@
-# import email
-# import imaplib
-
-# EMAIL = 'primusdiscordbot@gmail.com'
-# SERVER = 'imap.gmail.com'
-
-# f = open('gmailPassword.txt')
-# gmailPassword = f.read()
-# f.close()
-# print(gmailPassword)
-
-# mail = imaplib.IMAP4_SSL(SERVER)
-# mail.login(EMAIL, gmailPassword)
-# mail.select('inbox')
-# status, data = mail.search(None, 'ALL')
-# mail_ids = []
-# for block in data:
-#     mail_ids += block.split()
-
-# for i in mail_ids:
-#     status, data = mail.fetch(i, '(RFC822)')
-#     for response_part in data:
-#         if isinstance(response_part, tuple):
-#             message = email.message_from_bytes(response_part[1])
-#             mail_from = message['from']
-#             mail_subject = message['subject']
-#             if message.is_multipart():
-#                 mail_content = ''
-
-#                 for part in message.get_payload():
-#                     if part.get_content_type() == 'text/plain':
-#                         mail_content += part.get_payload()
-#             else:
-#                 mail_content = message.get_payload()
-#             print(f'From: {mail_from}')
-#             print(f'Subject: {mail_subject}')
-#             print(f'Content: {mail_content}')
-
 EMAIL = 'primusdiscordbot@gmail.com'
 SERVER = 'imap.gmail.com'
 
 f = open('gmailPassword.txt')
 gmailPassword = f.read()
 f.close()
-print(gmailPassword)
 
 def cb(cb_arg_list):
     response, cb_arg, error = cb_arg_list
@@ -57,9 +18,10 @@ def cb(cb_arg_list):
 
 import getpass, imaplib2
 M = imaplib2.IMAP4_SSL(SERVER)
-# M.LOGIN(getpass.getuser(), getpass.getpass())
 M.LOGIN(EMAIL, gmailPassword)
 M.SELECT(readonly=True)
+print("Going into idle...")
+M.idle(timeout=None)
 typ, data = M.SEARCH(None, 'ALL')
 for num in data[0].split():
     M.FETCH(num, '(RFC822)', callback=cb)
